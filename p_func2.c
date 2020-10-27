ad70cc2e304cd7f1c7b3c7e1041337abbfb18eb4
Nicolas <nicolasherrerac20@gmail.com>
2020-10-27 20:36:21 +0000

    version 1 print functions

---
+++ p_func2.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * p_percent - Function that prints a character to standard output
+ * @args: Variadic list of arguments
+ * Return: The number of characters printed
+ */
+
+int p_percent(__attribute__((unused))va_list list)
+{
+	_putchar('%');
+	return (1);
+}
