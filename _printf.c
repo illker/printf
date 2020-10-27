9a63ef2df14350143776053ed01bd17a65f357c3
Nicolas <nicolasherrerac20@gmail.com>
2020-10-27 20:35:42 +0000

    _print.c v3

--- _printf.c
+++ _printf.c
@@ -6,12 +6,12 @@
  */
 int _printf(const char *format, ...)
 {
-
 	va_list args;
 	int counter = 0, i, j;
 	f_print ops[] = {
 		{"c", p_character},
 		{"s", p_string},
+		{"%", p_percent},
 		{"i", p_int},
 		{"d", p_int},
 		{NULL, NULL}
@@ -26,6 +26,7 @@ int _printf(const char *format, ...)
 		{
 			_putchar(format[i]);
 			counter++;
+
 			i++;
 		}
 		if (format[i] != '\0')
@@ -39,6 +40,12 @@ int _printf(const char *format, ...)
 			if (format[i] == *(ops[j]).letter)
 			{
 				counter += ops[j].f(args);
+				break;
+			}
+			else
+			{
+				_putchar(37);
+				_putchar(format[i]);
 				break;
 			}
 		}
