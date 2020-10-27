2538edfaa7d457935c525ae1d03938a66e4fd13f
David Alzate <illker@hotmail.com>
2020-10-27 00:11:54 +0000

    firts version print main

---
+++ _printf.c
@@ -0,0 +1,45 @@
+include "holberton.h"
+/**
+ * _printf - print character to stdout
+ * @format: number of args
+ * Return: On success 1.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int counter1, counter2;
+	va_list args;
+
+	f_print ops[] = {
+		{"c", p_character},
+		{"s", p_string},
+		{"%", p_percent},
+		{"i", p_int},
+		{"d", p_int},
+		{NULL, NULL}
+	};
+	va_start(args, format);
+	if (!format || format[0] == '%' && format[1] == '\0')
+		return (-1);
+	counter1 = 0;
+	while (format && format[counter1])
+	{
+		if (format[counter1] == '%' || format[counter1] == 47)
+		{
+			if (format[counter1 + 1] == '%')
+				_putchar(format[counter1 + 1]);
+		}
+		counter2 = 0;
+		while (ops[counter2].format != NULL)
+		{
+			if (*(ops[counter2].fomat) == format[counter1])
+			{
+				ops[counter2].f(args);
+				break;
+			}
+			counter2++;
+		}
+		counter1++;
+	}
+	va_end(args);
+	return (counter2);
+}
