0b57b0b50c60e1728b40f505a298e71df745d13e
David Alzate <illker@hotmail.com>
2020-10-27 21:55:13 +0000

    printf version 8

--- _printf.c
+++ _printf.c
@@ -12,10 +12,8 @@ int _printf(const char *format, ...)
 
 	f_print ops[] = {
 		{"c", p_character}, {"s", p_string}, {"%", p_percent},
-		{"i", p_int}, {"d", p_int},
-		{NULL, NULL}
+		{"i", p_int}, {"d", p_int}, {NULL, NULL}
 	};
-
 	va_start(args, format);
 	if (format == NULL)
 		return (0);
@@ -34,14 +32,18 @@ int _printf(const char *format, ...)
 		for (j = 0; j < 5; j++)
 		{
 			if (format[i] == *(ops[j]).letter)
+			{
 				teamf = ops[j].f;
-			break;
+				break;
+			}
 		}
 		if (teamf != NULL)
 			counter += teamf(args);
 		else
+		{
 			_putchar(37);
-		_putchar(format[i]);
+			_putchar(format[i]);
+		}
 	}
 	va_end(args);
 	return (counter);
