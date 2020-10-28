23fdbd7f92ddd346ebb196be6dd0d865c97b2341
David Alzate <illker@hotmail.com>
2020-10-28 00:00:07 +0000

    Eye of the Tiger

--- holberton.h
+++ holberton.h
@@ -17,10 +17,10 @@ typedef struct f_print
 } f_print;
 
 int _printf(const char *format, ...);
-int p_character(va_list);
-int p_string(va_list);
-int p_percent(__attribute__((unused))va_list);
-int p_int(va_list);
+int p_character(va_list f_print);
+int p_string(va_list f_print);
+int p_percent(__attribute__((unused))va_list f_print);
+int p_int(va_list f_print);
 int countnumber(unsigned int n);
 int _putchar(char c);
 
