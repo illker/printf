f0f51101c802244b8f9d145ada440c1d7189f3b7
Nicolas <nicolasherrerac20@gmail.com>
2020-10-28 03:52:58 +0000

    p2functions v3

--- p_func2.c
+++ p_func2.c
@@ -11,3 +11,30 @@ int p_percent(__attribute__((unused))va_list args)
 	_putchar('%');
 	return (1);
 }
+
+/**
+* p_rot13 -  encodes a string using.
+* @s: pointer.
+* Return: count.
+*/
+int p_rot13(va_list args)
+{
+	int c, i, count = 0;
+	char *s;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	s = va_arg(args, char *);
+	for (c = 0; s[c] != '\0'; c++)
+		for (i = 0; l[i] != '\0'; i++)
+			if (s[c] == l[i])
+			{
+				count += _putchar(n[i]);
+				break;
+			}
+		if (i > 51)
+		{
+			count += _putchar(s[c]);
+		}
+	return (count);
+}
